657036256de5ea7e604f0276e7f1a52b8dc74966
haoyeu <bobbycookie213@gmail.com>
2020-11-28 22:02:36 +0800

    calc avg reviews

--- read.py
+++ read.py
@@ -1,13 +1,19 @@
 data = []
 count = 0
+total_data_len = 2
 with open('reviews.txt','r') as f:
 	for line in f:
 		data.append(line)
 		count +=1
 		if count % 1000 == 0:
 			print(len(data))
+	for len_per_data in f:
+		print(len(len_per_data))
+
 print(len(data))
 
-print(data[0])
-print('---------')
-print(data[1])
+sum_len = 0
+for d in data:
+	sum_len = sum_len + len(d)
+
+print(sum_len/len(data))
